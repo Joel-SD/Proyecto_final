f74aa0ae38a891dae1e11b37eee4d7c1f3cf333b
CesarJavier09 <72425776+CesarJavier09@users.noreply.github.com>
2022-07-06 13:24:34 -0500

    Se modifico la pantalla principal, se avanzó con la salida de datos... Los datos ahora se muestran en una ventana emergen... voy a hacer vida social, por la tarde sigo xD

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
+
     
+    $("#window-resultados").hide();
+    $("#detalle").hide();
     $("#fecha").val(function(){
         let fecha = new Date();
         fecha.setHours(fecha.getHours()-5);
@@ -19,22 +22,65 @@ $(document).ready(function(){
         if ($("#DRENAJE4").val() != "") {
             $("#TOTAL_BALANCE4").text(2000 - $("#DRENAJE4").val());
         }
-    })
 
-        $(".input").change(function() {
-    
-            var empty = false;
-            $('.input').each(function() {
-                if ($(this).val() == '') {
-                    empty = true;
-                }
-            });
-    
-            if (empty) {
-                $('#calcular').attr('disabled', 'disabled'); // updated according to http://stackoverflow.com/questions/7637790/how-to-remove-disabled-attribute-with-jquery-ie
-            } else {
-                $('#calcular').removeAttr('disabled'); // updated according to http://stackoverflow.com/questions/7637790/how-to-remove-disabled-attribute-with-jquery-ie
+        $("#detalle").show();
+    });
+
+    $("#detalle").click(function(){
+        $("#window-resultados").show();
+
+        $("#nombre_resultado").text($("#nombre").val());
+
+        let contTurbio =0;
+
+        if($("#condicion1").val()==2){
+            contTurbio++;
+        }
+        if($("#condicion2").val()==2){
+            contTurbio++;
+        }
+        if($("#condicion3").val()==2){
+            contTurbio++;
+        }
+        if($("#condicion4").val()==2){
+            contTurbio++;
+        }
+
+        console.log(contTurbio);
+
+        if($("TOTAL_BALANCE_FINAL")>2000){
+            $("#resultadoBalance").text("ALERTA: Excesiva retención de líquidos. Consulte de inmediato con su nefrólogo\nBalance Final del día =" +  $("#TOTAL_BALANCE_FINAL").val());
+            alert("Excesiva retención de líquidos. Consulte de inmediato con su nefrólogo");
+            if((contTurbio) >= 2){
+                alert("ALERTA: Consulte de inmediato con su néfrologo y programe su cita en la unidad de diálisis");
+            }
+        }else if($("TOTAL_BALANCE_FINAL")>0){
+            $("#resultadoBalance").text("Retención de líquidos considerable. En caso de presentarse esta situación por más de dos días consecutivos, llame y consulte con la unidad hospitalaria de Diálisis\nBalance Final del día" + $("#TOTAL_BALANCE_FINAL").val());
+            if((contTurbio) >= 2){
+                alert("ALERTA: Consulte de inmediato con su néfrologo y programe su cita en la unidad de diálisis");
+            }
+        }else{
+            $("#resultadoBalance").text("Balance Hídrico Favorable. Condición normal, no hay retención de líquidos.");
+            if((contTurbio) >= 2){
+                alert("ALERTA: Consulte de inmediato con su néfrologo y programe su cita en la unidad de diálisis");
+            }
+        }
+    });
+
+    $(".input").change(function() {
+
+        var empty = false;
+        $('.input').each(function() {
+            if ($(this).val() == '') {
+                empty = true;
             }
         });
+
+        if (empty) {
+            $('#calcular').attr('disabled', 'disabled'); 
+        } else {
+            $('#calcular').removeAttr('disabled'); 
+        }
+    });
 })
 
