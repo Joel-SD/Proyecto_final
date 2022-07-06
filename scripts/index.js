52dd47fe08685a7ee97791051227d76f64f2433f
CesarJavier09 <72425776+CesarJavier09@users.noreply.github.com>
2022-07-06 18:19:34 -0500

    Algunos cambios xD

--- scripts/index.js
+++ scripts/index.js
@@ -9,29 +9,37 @@ $(document).ready(function(){
         return fecha.toJSON().slice(0,19)
     });
 
+    let sumaDrenaje = 0;
+
+    let contTurbio =0;
+
     $("#calcular").click(function(){
         if ($("#DRENAJE1").val() != "") {
-            $("#TOTAL_BALANCE1").text(2000 - $("#DRENAJE1").val());
+            sumaDrenaje += Number($("#DRENAJE1").val());
+            $("#TOTAL_BALANCE1").text(Number(2000 - $("#DRENAJE1").val()) + " ml");
         }
         if ($("#DRENAJE2").val() != "") {
-            $("#TOTAL_BALANCE2").text(2000 - $("#DRENAJE2").val());
+            sumaDrenaje += Number($("#DRENAJE2").val());
+            $("#TOTAL_BALANCE2").text(Number(2000 - $("#DRENAJE2").val()) + " ml");
         }
         if ($("#DRENAJE3").val() != "") {
-            $("#TOTAL_BALANCE3").text(2000 - $("#DRENAJE3").val());
+            sumaDrenaje += Number($("#DRENAJE3").val());
+            $("#TOTAL_BALANCE3").text(Number(2000 - $("#DRENAJE3").val()) + " ml");
         }
         if ($("#DRENAJE4").val() != "") {
-            $("#TOTAL_BALANCE4").text(2000 - $("#DRENAJE4").val());
+            sumaDrenaje += Number($("#DRENAJE4").val());
+            $("#TOTAL_BALANCE4").text(Number(2000 - $("#DRENAJE4").val()) + " ml");
         }
 
+        $("#TOTAL_INFUSION").html("<p>8000 ml</p>");
+
+        $("#TOTAL_DRENAJE").html("<p>"+sumaDrenaje+" ml</p>");
+
+        $("#TOTAL_BALANCE_FINAL").html("<p id=\"sumaTotalBalance\">"+Number(8000-sumaDrenaje)+"</p><p> ml</p>");
+
+        sumaDrenaje = 0;
+
         $("#detalle").show();
-    });
-
-    $("#detalle").click(function(){
-        $("#window-resultados").show();
-
-        $("#nombre_resultado").text($("#nombre").val());
-
-        let contTurbio =0;
 
         if($("#condicion1").val()==2){
             contTurbio++;
@@ -46,23 +54,39 @@ $(document).ready(function(){
             contTurbio++;
         }
 
-        console.log(contTurbio);
+        if(contTurbio >= 2){
+            alert("Consulte de inmediato con su néfrologo y programe su cita en la unidad de diálisis");
+        }
 
-        if($("TOTAL_BALANCE_FINAL")>2000){
-            $("#resultadoBalance").text("ALERTA: Excesiva retención de líquidos. Consulte de inmediato con su nefrólogo\nBalance Final del día =" +  $("#TOTAL_BALANCE_FINAL").val());
+        console.log(contTurbio);
+    });
+
+    $("#detalle").click(function(){
+        $("#window-resultados").show();
+
+        $("#nombre_resultado").text($("#nombre").val());
+
+        if($("#sumaTotalBalance").text()>2000){
             alert("Excesiva retención de líquidos. Consulte de inmediato con su nefrólogo");
             if((contTurbio) >= 2){
-                alert("ALERTA: Consulte de inmediato con su néfrologo y programe su cita en la unidad de diálisis");
+                alert("Consulte de inmediato con su néfrologo y programe su cita en la unidad de diálisis");
+                $("#resultadoBalance").html("<p><strong>ALERTA</strong>: Consulte de inmediato con su néfrologo y programe su cita en la unidad de diálisis.</p><p><strong>ALERTA</strong>: Excesiva retención de líquidos. Consulte de inmediato con su nefrólogo</p><p>Balance Final del día = " +  $("#sumaTotalBalance").text() + " ml</p>");
+            }else{
+                $("#resultadoBalance").html("<p><strong>ALERTA</strong>: Excesiva retención de líquidos.</p><p>Consulte de inmediato con su nefrólogo</p><p>Balance Final del día = " +  $("#sumaTotalBalance").text() + " ml</p>");
             }
-        }else if($("TOTAL_BALANCE_FINAL")>0){
-            $("#resultadoBalance").text("Retención de líquidos considerable. En caso de presentarse esta situación por más de dos días consecutivos, llame y consulte con la unidad hospitalaria de Diálisis\nBalance Final del día" + $("#TOTAL_BALANCE_FINAL").val());
+        }else if($("#sumaTotalBalance").text()>0){
             if((contTurbio) >= 2){
-                alert("ALERTA: Consulte de inmediato con su néfrologo y programe su cita en la unidad de diálisis");
+                alert("Consulte de inmediato con su néfrologo y programe su cita en la unidad de diálisis");
+                $("#resultadoBalance").html("<p><strong>ALERTA</strong>: Consulte de inmediato con su néfrologo y programe su cita en la unidad de diálisis.</p><p>Retención de líquidos considerable. En caso de presentarse esta situación por más de dos días consecutivos, llame y consulte con la unidad hospitalaria de Diálisis</p><p>Balance Final del día = " + $("#sumaTotalBalance").text() + " ml</p>");
+            }else{
+                $("#resultadoBalance").html("<p>Retención de líquidos considerable. En caso de presentarse esta situación por más de dos días consecutivos, llame y consulte con la unidad hospitalaria de Diálisis</p><p>Balance Final del día = " + $("#sumaTotalBalance").text() + " ml</p>");
             }
         }else{
-            $("#resultadoBalance").text("Balance Hídrico Favorable. Condición normal, no hay retención de líquidos.");
             if((contTurbio) >= 2){
-                alert("ALERTA: Consulte de inmediato con su néfrologo y programe su cita en la unidad de diálisis");
+                alert("Consulte de inmediato con su néfrologo y programe su cita en la unidad de diálisis");
+                $("#resultadoBalance").html("<p><strong>ALERTA</strong>: Consulte de inmediato con su néfrologo y programe su cita en la unidad de diálisis.</p><p>Balance Hídrico Favorable. Condición normal, no hay retención de líquidos.</p>");
+            }else{
+                $("#resultadoBalance").html("<p>Balance Hídrico Favorable. Condición normal, no hay retención de líquidos.</p>");
             }
         }
     });
@@ -82,5 +106,14 @@ $(document).ready(function(){
             $('#calcular').removeAttr('disabled'); 
         }
     });
+
+    $("#cerrar-detalle").click(function(){
+        $("#window-resultados").hide();
+    })
+
+    $("#grafica").click(function(){
+        var ventana = window.open('./grafico.html');
+        ventana.focus(); 
+    }).css('cursor', 'pointer');
 })
 
